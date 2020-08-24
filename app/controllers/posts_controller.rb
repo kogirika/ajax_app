9be1e7d70f911eb9f2ee1925574f7565b2cf6c51
kogirika <kogirika12@gmail.com>
2020-08-24 17:05:31 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,10 @@ class PostsController < ApplicationController
 
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content], checked: false)
+    render json:{post: post}
+    # Post.create(content: params[:content])
+    # redirect_to action: :index
   end
 
   def checked
